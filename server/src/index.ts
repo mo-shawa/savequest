b73dd3fb311331e51f07d85ad96e7930a25a2a1d
moshawa <moshawa0@gmail.com>
2023-09-09 12:12:06 -0400

    setup prettier

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
-import express from "express";  
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/", (req,res) => {
-    res.send("Hello dork");
+app.get('/', (req, res) => {
+	res.send('Hello dork')
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+})
